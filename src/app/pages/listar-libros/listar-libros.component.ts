c71ddd7aa8a42620c7a857cd55b2f322ab8fedba
HelenVr <helenvegaresendiz34@gmail.com>
2025-07-02 08:15:40 -0600

    listar-libros

--- src/app/pages/listar-libros/listar-libros.component.ts
+++ src/app/pages/listar-libros/listar-libros.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { LibroService } from '../../services/libro.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listar-libros',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listar-libros.component.html',
   styleUrl: './listar-libros.component.css'
 })
-export class ListarLibrosComponent {
+export class ListarLibrosComponent implements OnInit{
+
+  //propiedades
+  libros: any = [];
+
+  constructor(private libroService: LibroService){
+    this.getLibros();
+  }
+  ngOnInit(): void {}
+
+  //método que hace la petición al service para obtener los empleados 
+  getLibros(){
+    this.libroService.getLibros().subscribe((data)=>{
+      this.libros = data
+    });
+  }
+
+  //método para eliminar un empleado
+  eliminarLibro(libro:any,index:any){
+    if(window.confirm('¿Estás seguro de que deseas eliminar este libro?')){
+      this.libroService.eliminarLibro(libro._id).subscribe((data)=>{
+        this.libros.splice(index, 1);
+      })
+    }
+  }
 
 }
